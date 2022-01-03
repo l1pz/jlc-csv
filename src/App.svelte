a93c101eb70091e32da1752741bc47a555a86463
Máté Molnár <mmateka89@gmail.com>
2022-01-03 21:05:19 +0100

    add bulma

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,13 @@
-<h1>Hello World!</h1>
+<script>
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"
+  />
+</svelte:head>
+
+<div class="container">
+  <h1 class="title is-2 mx-2 mt-4">JLC CSV Helper</h1>
+</div>
